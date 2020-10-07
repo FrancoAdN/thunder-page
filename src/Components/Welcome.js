02ca49606a3d2829c1cb920cf91e94c12a19863a
tomasvera1 <tomasveratec@gmail.com>
2020-10-06 22:53:35 -0300

    add

--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import banner from '../assets/images/banner.gif'
 
 export default function Welcome() {
@@ -12,12 +13,14 @@ export default function Welcome() {
                 <div className="container">
                     <div className="row">
                         <div className="offset-xl-3 col-xl-6 offset-lg-2 col-lg-8 col-md-12 col-sm-12">
-                            <h1> El eloboost mas <strong> confiable </strong><br /> y rapido de <strong>
+                            <h1> El eloboost mas <strong> confiable </strong><br /> y rápido de <strong>
                                 latinoamérica</strong></h1>
                             <p> Proporcionamos el mejor servicio de eloboost y coaching a nuestros clientes, brindando alta
                             calidad y rendimiento
                             en el desempeño de nuestros boosters. </p>
-                            <a href="#features" className="main-button-slider"> ¡Conocenos! </a>
+                            <Link to={"/boost"}>
+                            <a href="" className="main-button-slider"> ¡Contratar! </a>
+                            </Link>
                         </div>
                     </div>
                 </div>
